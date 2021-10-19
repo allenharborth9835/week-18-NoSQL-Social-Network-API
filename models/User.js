50275654e6531594f7a59949ded42739e78e68ef
Harborth, Allen <allenharborth9835@gmail.com>
2021-10-18 21:55:44 -0500

    add user schema and project layout

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+  username:{
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
+  },
+  thoughts:[],
+  freinds: [],
+},
+{
+  toJSON:{
+    virtuals: true
+  },
+  id: false
+}
+)
+
+userSchema.virtual('freindCount').get(function(){
+  return this.freinds.length()
+})
+
+const User = model('User', userSchema);
+
+module.exports = User;
